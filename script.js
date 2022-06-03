eac73cf197a426710b5dff7716ede1da1cb23a11
Sakika0704 <sakikahamada74u@gmail.com>
2022-06-03 21:12:38 +0900

    20220603

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-console.log(1+4)
-console.log(1+4)
+// function showMessage() {
+//     alert( 'Hello everyone!' );
+//   }
+  
+//   showMessage();
+//   showMessage();
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+ let ask = (question, yes, no) => {
+    if (confirm(question)) yes()
+    else no();
+ }
+
+ ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+  );
